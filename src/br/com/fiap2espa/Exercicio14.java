ac098306b6ce0abef34f1085ff00fffce99eb925
guivital1 <guivitalrodrigues@gmail.com>
2025-09-05 20:31:01 -0300

    Add Exercicio14.java for temperature conversion

--- src/br/com/fiap2espa/Exercicio14.java
+++ src/br/com/fiap2espa/Exercicio14.java
@@ -1,6 +1,6 @@
 public class ConversaoTemperatura {
 
-    // Função que recebe Fahrenheit e retorna Celsius
+
     public static double fahrenheitParaCelsius(double f) {
         return (f - 32) * 5 / 9;
     }
@@ -10,4 +10,5 @@ public class ConversaoTemperatura {
         double celsius = fahrenheitParaCelsius(fahrenheit);
         System.out.println(fahrenheit + "°F = " + celsius + "°C");
     }
+
 }
